3c8233c7a58d162ec2990c30de6f98b9ffa299bc
claus.e.andersen <clan@dtu.dk>
2020-04-25 15:52:44 +0200

    Again

--- R/subscript-groups-panel-function-demo-function.R
+++ R/subscript-groups-panel-function-demo-function.R
@@ -23,7 +23,88 @@
 #' @author Claus E. Andersen
 #' @return A Lattice plot
 #' @export panel.function.demo
-panel.function.demo <- function(){
+panel.function.demo <- 
+  function () 
+  {
+    print("Welcome to panel.function.demo")
+    print("Clear df.outside (was set to NULL)")
+    assign("df.outside", NULL, envir = sys.frame(0))
+      
+    plt1 <- xyplot(Sepal.Length ~ Sepal.Width | Species, par.strip.text = list(cex = 1.5), 
+                   data = iris, 
+                   panel = function(x, y, subscripts, groups, ...) {
+                     panel.xyplot(x, y, ...)
+                   } # panel function
+                   ) # xyplot
+    
+    
+    
+        plt2 <- xyplot(Sepal.Length ~ Sepal.Width | Species, data = iris, 
+                   panel = function(x, y, subscripts, groups, ...) {
+                    
+                     
+                     # Tell me where we are
+                     print(paste("Panel no. = ", panel.number()))
+                     print(paste("Row no. = ", current.row()))
+                     print(paste("Column no. = ", current.column()))
+                     
+                     cex0 <- 0.8
+                     grid::grid.text(paste("SD =", sprintf("%.5f", 
+                                                           round(sd(y), 5))), x = grid::unit(0.1, "npc"), 
+                                     y = grid::unit(0.85, "npc"), just = 0, 
+                                     gp = grid::gpar(cex = cex0))
+                     
+                     # Demonstration of the use of subscripts:
+                     x.Specises <- paste(unique(iris$Species[subscripts]), collapse = "")
+                     print(x.Specises)
+                     
+                     if (missing(groups)) {
+                       panel.xyplot(x, y, ...)
+                     }
+                     else {
+                       panel.superpose(x, y, subscripts, groups, ...)
+                     }
+                     
+                     
+                     # How to get data in and out of the panel function (here using df.out/ df.outside
+                     df0 <- data.frame(x.mean = mean(x), y.mean = mean(y), 
+                                       panel.no = panel.number(), Specises = x.Specises, 
+                                       N = length(x))
+                     
+                     if(!exists("df.outside", envir = sys.frame(0))){ 
+                       assign("df.outside", NULL, envir = sys.frame(0))
+                     }            
+  
+                    
+                    df.out <- get("df.outside", envir = sys.frame(0))
+                     
+                     if (is.null(df.out)) {
+                       df.out <- df0
+                     }
+                     else {
+                       df.out <- rbind(df.out, df0)
+                     }
+                    
+                     assign("df.outside", df.out, envir = sys.frame(0))
+                   }  # panel function
+                   )# xyplot
+
+    print(plt1)
+    print(plt2)
+    plt3 <- update(plt2, groups = iris$Species)
+    plt3
+    list(plt1, plt2, plt3)
+    txtplot(get("df.outside", envir = sys.frame(0))[1:8,])
+    print(df.outside)
+    print("The dataframe df.outside is now available from the top environment.")
+    print("It contains data collected within the panel function!")
+    print("ByeBye from panel.function.demo")
+  } # panel.function.demo
+
+
+  
+  
+  function(){
 names(iris)
 
 
